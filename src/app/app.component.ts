57d15208459192b1cb786b7bcd47f8df77f0c918
bhupendra <bhupendra.choudhary@mindruby.com>
2023-02-13 14:42:25 +0530

    Angular Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-ang';
+
+// constructor(){
+//   setTimeout(() => {
+//     this.title = "Welcome here";
+//   }, 2000);
+// }
 }
